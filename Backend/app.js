53530de8c64982226fcc5dfd7a4b6120fdadd7ef
Gaurav Gupta <gaurav33dev@gmail.com>
2025-05-19 17:40:07 +0530

    server-is-started

--- Backend/app.js
+++ Backend/app.js
@@ -7,6 +7,8 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import hpp from "hpp";
 import mongoSanitize from "express-mongo-sanitize";
+import path from "path";
+import { fileURLToPath } from "url";
 import xss from "xss-clean";
 import { corsOptions } from "./src/config/corsOptions.config.js";
 import { errorHandler } from "./src/utils/errorHandler.utils.js";
@@ -20,6 +22,8 @@ dotenv.config();
 // Body Parser Middleware
 app.use(express.json({limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb"  }));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
@@ -33,10 +37,10 @@ app.use(helmet());
 app.use(hpp()); 
 
 // Data sanitization against NoSQL query injection
-app.use(mongoSanitize()); 
+// app.use(mongoSanitize(mongoSanitizeOptions));
 
 // Data sanitization against XSS
-app.use(xss()); 
+// app.use(xss()); 
 
 // loogging middleware
 if (process.env.NODE_ENV === "development") {
@@ -53,9 +57,6 @@ app.use("/api/v1", limiter);
 // handling 404-notFound error
 app.use(notFoundHandler);
 app.use(errorHandler);
-
-
-
 
 
 app.listen(process.env.PORT, () => {
--- Backend/src/config/db.config.js
+++ Backend/src/config/db.config.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(`${process.env.MONGO_URI}/trimly`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(`${process.env.MONGO_URI}/trimly`);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
